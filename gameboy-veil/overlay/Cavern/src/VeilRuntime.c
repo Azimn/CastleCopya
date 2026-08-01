0f364716342c6ba5cc7abb260cfc5db3ab4011e3
Azimn <jratican@gmail.com>
2026-07-31 20:02:36 -0500

    Connect visible menu renderer to Veil runtime

--- gameboy-veil/overlay/Cavern/src/VeilRuntime.c
+++ gameboy-veil/overlay/Cavern/src/VeilRuntime.c
@@ -1,10 +1,12 @@
 #include "VeilRuntime.h"
 #include "VeilMenu.h"
+#include "VeilMenuView.h"
 #include "VeilCampaign.h"
 #include "Keys.h"
 
 void VeilRuntime_Init(void) {
     VeilMenu_Init();
+    VeilMenuView_Init();
     if (!VeilCampaign_IsValid()) {
         VeilCampaign_NewGame();
     }
@@ -14,8 +16,10 @@ UINT8 VeilRuntime_Update(void) {
     if (KEY_TICKED(J_START)) {
         if (veil_menu.is_open) {
             VeilMenu_Close();
+            VeilMenuView_Hide();
         } else {
             VeilMenu_Open();
+            VeilMenuView_Show();
         }
         return veil_menu.is_open;
     }
@@ -35,6 +39,12 @@ UINT8 VeilRuntime_Update(void) {
     }
     if (KEY_TICKED(J_B)) {
         VeilMenu_Close();
+        VeilMenuView_Hide();
+        return 0U;
+    }
+
+    if (veil_menu.dirty) {
+        VeilMenuView_Render();
     }
 
     return veil_menu.is_open;

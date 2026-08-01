dd137c5d8b5d5d771e736688668e1d84964e3c53
Azimn <jratican@gmail.com>
2026-07-31 20:01:38 -0500

    Add Veil menu view interface

---
+++ gameboy-veil/overlay/Cavern/include/VeilMenuView.h
@@ -0,0 +1,11 @@
+#ifndef VEIL_MENU_VIEW_H
+#define VEIL_MENU_VIEW_H
+
+#include <asm/gbz80/types.h>
+
+void VeilMenuView_Init(void);
+void VeilMenuView_Show(void);
+void VeilMenuView_Hide(void);
+void VeilMenuView_Render(void);
+
+#endif
